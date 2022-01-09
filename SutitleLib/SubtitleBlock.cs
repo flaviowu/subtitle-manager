73900110f1265be4308a7a6a18536c44b3a71cbd
Flavio Wu <flaviowu@gmail.com>
2022-01-09 20:07:07 -0300

    add getters and setters to subtitleBlocks

--- SutitleLib/SubtitleBlock.cs
+++ SutitleLib/SubtitleBlock.cs
@@ -2,22 +2,46 @@
 {
     public class SubtitleBlock
     {
-        public string BlockNumber;
-        public string StartTime;
-        public string EndTime;
-        public string TextContent;
+        private string _blockNumber;
+        private string _startTime;
+        private string _endTime;
+        private string _textContent;
 
         public SubtitleBlock(string blockNumber, string startTime, string endTime, string textContent)
         {
-            BlockNumber = blockNumber;
-            StartTime = startTime;
-            EndTime = endTime;
-            TextContent = textContent;
+            _blockNumber = blockNumber;
+            _startTime = startTime;
+            _endTime = endTime;
+            _textContent = textContent;
+        }
+
+        public string BlockNumber
+        {
+            get => this._blockNumber;
+        }
+
+        public string StartTime
+        {
+            get => this._startTime;
+            set => this._startTime = value;
+        }
+
+        public string EndTime
+        {
+            get => this._endTime;
+            set => this._endTime = value;
+        }
+
+        public string TextContext
+        {
+            get => this._textContent;
+            set => this._textContent = value;
         }
 
         public override string ToString()
         {
-            return $"{this.BlockNumber}\n{this.StartTime} --> {this.EndTime}\n{this.TextContent}\n";
+            return $"{this._blockNumber}\n{this._startTime} --> {this._endTime}\n{this._textContent}\n";
         }
+
     }
 }
