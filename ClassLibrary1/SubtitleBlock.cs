d96620b56a2440547059e80804dc8b269a836d53
Flavio Wu <flaviowu@gmail.com>
2022-01-09 15:19:58 -0300

    1st commit

---
+++ ClassLibrary1/SubtitleBlock.cs
@@ -0,0 +1,23 @@
+﻿namespace SubtitleLib
+{
+    public class SubtitleBlock
+    {
+        public string BlockNumber;
+        public string StartTime;
+        public string EndTime;
+        public string TextContent;
+
+        public SubtitleBlock(string blockNumber, string startTime, string endTime, string textContent)
+        {
+            BlockNumber = blockNumber;
+            StartTime = startTime;
+            EndTime = endTime;
+            TextContent = textContent;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.BlockNumber}\n{this.StartTime} --> {this.EndTime}\n{this.TextContent}\n";
+        }
+    }
+}
---
+++ ClassLibrary1/Subtitles.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SubtitleLib
+{
+    public class Subtitles
+    {
+        public ArrayList Parse(string subtitleString)
+        {
+
+            Regex rx = new Regex(
+          @"(?<blockNumber>\d+)\r\n(?<startTime>\d{2}\:\d{2}\:\d{2},\d{3}) --\> " +
+          @"(?<endTime>\d{2}\:\d{2}\:\d{2},\d{3})\r\n(?<textContent>[\s\S]*?\r\n)",
+          RegexOptions.Compiled | RegexOptions.ECMAScript);
+
+            ArrayList subtitleBlocksArray = new ArrayList();
+            foreach (Match match in rx.Matches(subtitleString))
+            {
+                SubtitleBlock subtitleBlock = new SubtitleBlock(
+                    match.Groups["blockNumber"].Value,
+                    match.Groups["startTime"].Value,
+                    match.Groups["endTime"].Value,
+                    match.Groups["textContent"].Value
+                    );
+                subtitleBlocksArray.Add(subtitleBlock);
+
+            }
+            return subtitleBlocksArray;
+        }
+
+        public static ArrayList Offset(ArrayList subtitles, double offsetTime)
+        {
+            foreach (SubtitleBlock block in subtitles)
+            {
+                block.StartTime = String.Format("{0:HH\\:mm\\:ss\\,fff}", DateTime.Parse(block.StartTime.Replace(",", ".")).AddSeconds(offsetTime));
+                block.EndTime = String.Format("{0:HH\\:mm\\:ss\\,fff}", DateTime.Parse(block.EndTime.Replace(",", ".")).AddSeconds(offsetTime));
+            }
+            return subtitles;
+        }
+    }
+}
