8902289589ea160bb2d82dc4e101a0193d0f3f71
Flavio Wu <flaviowu@gmail.com>
2022-01-09 19:01:16 -0300

    create Tests

---
+++ SubtitleManagerTests/SubtitlesTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SubtitleLib;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubtitleLib.Tests
+{
+    [TestClass()]
+    public class SubtitlesTests
+    {
+        [TestMethod()]
+        public void ParseTest_Output()
+        {
+            string subtitleString = "1\n" +
+                "00:00:00,000 --> 00:00:03,000\n" +
+                "Some Text and morr text to test.\n";
+            ArrayList subtitles = Subtitles.Parse(subtitleString);
+            Assert.IsNotNull(subtitles);
+        }
+
+        [TestMethod()]
+        public void ParseTest_Output_values()
+        {
+            string subtitleString = "1\n00:00:00,000 --> 00:00:03,000\nSome Text and more text to test.\n";
+            ArrayList subtitles = Subtitles.Parse(subtitleString);
+            var ooo = subtitles[0];
+            // SubtitleBlock blocao = new SubtitleBlock("1", "00:00:00,000", "00:00:03,000", "Some Text and more text to test.");
+            bool asa = true;
+            Assert.IsTrue(asa);
+        }
+    }
+}
